c706343070ae1403cd6f7369c6eddf01aeccba66
Leonidas Avdelas <avdelasleonidas@gmail.com>
2024-06-02 21:52:35 +0300

    Add an exception for invalid stops

---
+++ src/main/java/gr/iccs/smart/mobility/boatStop/InvalidBoatStopException.java
@@ -0,0 +1,7 @@
+package gr.iccs.smart.mobility.boatStop;
+
+public class InvalidBoatStopException extends RuntimeException {
+    InvalidBoatStopException(String message) {
+        super(message);
+    }
+}
