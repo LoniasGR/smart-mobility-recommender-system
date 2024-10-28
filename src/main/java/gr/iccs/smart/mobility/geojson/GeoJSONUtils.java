e81fdd48e695a3c6b2d1c57c37b3269b81816358
Leonidas Avdelas <avdelasleonidas@gmail.com>
2024-10-28 21:38:33 +0200

    Minor fixes

--- src/main/java/gr/iccs/smart/mobility/geojson/GeoJSONUtils.java
+++ src/main/java/gr/iccs/smart/mobility/geojson/GeoJSONUtils.java
@@ -11,9 +11,10 @@ public class GeoJSONUtils {
     public static Feature createVehicleFeature(Vehicle v) {
         return createVehicleFeature(VehicleDTO.fromVehicle(v));
     }
+
     public static Feature createVehicleFeature(VehicleDTO v) {
         Feature f = new Feature();
-        f.setGeometry(new gr.iccs.smart.mobility.geojson.Point(v.location().longitude(), v.location().latitude()));
+        f.setGeometry(new gr.iccs.smart.mobility.geojson.Point(v.location().y(), v.location().x()));
 
         f.getProperties().put("type", v.type().toString());
         f.getProperties().put("id", v.id().toString());
@@ -41,14 +42,15 @@ public class GeoJSONUtils {
     public static Feature createPointFeature(Point point, String symbol) {
         return createPointFeature(point, symbol, null);
     }
+
     public static Feature createPointFeature(Point p, String symbol, String color) {
         Feature f = new Feature();
         f.setGeometry(new gr.iccs.smart.mobility.geojson.Point(p.y(), p.x()));
 
-        if(symbol != null) {
+        if (symbol != null) {
             f.getProperties().put("marker-symbol", symbol);
         }
-        if(color != null) {
+        if (color != null) {
             f.getProperties().put("marker-color", color);
         }
         return f;
