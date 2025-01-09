be4f5d91c625c11925f89954b244cf9aa3f8d172
Leonidas Avdelas <avdelasleonidas@gmail.com>
2025-01-09 12:13:52 +0200

    Set json properties of scenario file

--- src/main/java/gr/iccs/smart/mobility/scenario/ScenarioDTO.java
+++ src/main/java/gr/iccs/smart/mobility/scenario/ScenarioDTO.java
@@ -2,11 +2,16 @@ package gr.iccs.smart.mobility.scenario;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import gr.iccs.smart.mobility.pointsOfInterest.PortDTO;
 import gr.iccs.smart.mobility.vehicle.BoatDTO;
 import gr.iccs.smart.mobility.vehicle.CarDTO;
 import gr.iccs.smart.mobility.vehicle.ScooterDTO;
 
-public record ScenarioDTO(List<PortDTO> ports, List<CarDTO> cars, List<ScooterDTO> scooters, List<BoatDTO> boats) {
+public record ScenarioDTO(List<PortDTO> ports,
+                @JsonProperty("passenger_cars") List<CarDTO> cars,
+                List<ScooterDTO> scooters,
+                @JsonProperty("sea_vessels") List<BoatDTO> boats) {
 
 }
