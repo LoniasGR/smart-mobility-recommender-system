73718b3e346a902e50225e38b1645e741c430669
Leonidas Avdelas <avdelasleonidas@gmail.com>
2025-10-29 15:11:30 +0200

    Disable MQTT by default

--- src/main/java/gr/iccs/smart/mobility/config/FeatureFlags.java
+++ src/main/java/gr/iccs/smart/mobility/config/FeatureFlags.java
@@ -1,14 +1,12 @@
 package gr.iccs.smart.mobility.config;
 
 import org.togglz.core.Feature;
-import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 
 public enum FeatureFlags implements Feature {
 
     @Label("Vehicle Clustering")
     CLUSTERING,
-    @EnabledByDefault
     @Label("MQTT Integration")
     MQTT;
 }
